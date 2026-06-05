5844c6f40f592c9303a3577b72d9af4c83165f35
DryRye. <deangellocampanella5@gmail.com>
2026-06-05 04:00:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent 
+    ] 
+});
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
@@ -19,13 +26,24 @@ app.post('/api/submit', (req, res) => {
     res.status(403).json({ ok: false, result: "PORTAL_CLOSED" });
 });
 
+// Help command text responder
+client.on('messageCreate', async (message) => {
+    // Ignore other bots so it doesn't cause loops
+    if (message.author.bot) return;
+
+    // Check if the message is exactly /help
+    if (message.content.toLowerCase() === '/help') {
+        await message.reply(".....uh.....um....mmmmmm, my master aint set me up gng, srry bout dat, TELL EM DOH>;3");
+    }
+});
+
 // Custom Status Setup
 client.once('ready', () => {
     console.log(`🤖 Logged in as ${client.user.tag}!`);
     
     client.user.setPresence({
         activities: [{ 
-            name: 'bot hosted by RyesBots :3', 
+            name: 'bot hosted by RyesBots', 
             type: 3
         }],
         status: 'online', 
